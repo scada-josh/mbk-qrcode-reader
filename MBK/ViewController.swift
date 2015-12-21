90f4f9944456d2ece6ff796347fc678c0007eca2
anusorn <scada.josh@gmail.com>
2015-12-21 23:26:22 +0700

    setup cocoapods for MBK - QRCode reader

---
+++ MBK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MBK
+//
+//  Created by Macbook Air on 12/21/2558 BE.
+//  Copyright © 2558 Code-Aholic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
